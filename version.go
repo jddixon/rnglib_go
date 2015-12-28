c29a859bea87491fb8d599c32b25e34186337818
Jim Dixon <jddixon@gmail.com>
2015-12-27 20:37:12 -0800

    fix dead link

--- version.go
+++ version.go
@@ -1,10 +1,6 @@
 package rnglib_go
 
-// The version MUST consist of three parts separated by dots,
-// with each part being one or two digits.  It is converted
-// into a uint32 in in_handler.go init()
 const (
-	// the version number tracked in CHANGES
-	VERSION      = "0.5.4"
-	VERSION_DATE = "2015-11-18"
+	VERSION      = "0.5.5"
+	VERSION_DATE = "2015-12-27"
 )
